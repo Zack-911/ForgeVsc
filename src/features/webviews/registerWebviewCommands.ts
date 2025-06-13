7276e20a876beecf88c868dab2b6df22d212a3d8
Zack <muhammadbinasim471@gmail.com>
2025-06-13 18:07:52 +0500

    Added index gen

--- src/features/webviews/registerWebviewCommands.ts
+++ src/features/webviews/registerWebviewCommands.ts
@@ -2,11 +2,13 @@ import * as vscode from "vscode";
 import { openDocs } from "./openDocs";
 import { openSlashCommand } from "./openSlashCommand";
 import { openPermission } from "./openPermissionGenerator";
+import { openIndexGen } from "./openIndexGen";
 
 export function registerWebviewCommands(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("forge.docs", openDocs),
     vscode.commands.registerCommand("forge.slashCommand", openSlashCommand),
-    vscode.commands.registerCommand("forge.permission", openPermission)
+    vscode.commands.registerCommand("forge.permission", openPermission),
+    vscode.commands.registerCommand("forge.indexGen", openIndexGen)
   );
 }
